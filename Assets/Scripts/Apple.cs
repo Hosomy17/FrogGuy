665ecb4f9adda2132b0b7474c559db7983bc296a
marcosduarte94 <mvcd.pje22@uea.edu.br>
2022-08-07 13:52:49 -0400

    Otimizações finais, aula final

--- Assets/Scripts/Apple.cs
+++ Assets/Scripts/Apple.cs
@@ -6,32 +6,29 @@ using UnityEngine;
 
 public class Apple : MonoBehaviour
 {
-    private SpriteRenderer sr;
+    private Animator anim;
     private CircleCollider2D circle;
+    
     [SerializeField]
     private AudioClip _pickSFX;
-
-    public GameObject collected;
+    
     public int Score;
     
     void Start()
     {
-        sr = GetComponent<SpriteRenderer>();
-        circle = GetComponent<CircleCollider2D>();
+        TryGetComponent(out anim);
+        TryGetComponent(out circle);
     }
 
     private void OnTriggerEnter2D(Collider2D collider)
     {
         if (collider.gameObject.tag == "Player")
         {
-            sr.enabled = false;
             circle.enabled = false;
-            collected.SetActive(true);
-
-            GameController.instance.totalScore += Score;
-
-            AudioSource.PlayClipAtPoint(_pickSFX, Vector3.back * 10);
+            anim.Play("collected");
             
+            GameController.instance.totalScore += Score;
+            AudioSource.PlayClipAtPoint(_pickSFX, Vector3.back * 10);
             Invoke("DisableApple", 0.25f);
         }
     }
